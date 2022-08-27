43fd4693b9556e08556f5def4c53934004e7844e
Fredrik Noring <noring@nocrew.org>
2022-08-27 09:10:24 +0200

    Add file storage

---
+++ include/internal/storage.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2021 Fredrik Noring
+ */
+
+#ifndef INTERNAL_STORAGE_H
+#define INTERNAL_STORAGE_H
+
+#include "internal/types.h"
+
+/**
+ * struct storage_file - file storage
+ * @path: file path
+ * @size: file size in bytes, not counting the NUL termination
+ * @data: file data, always NUL terminated
+ */
+struct storage_file {
+	const char *path;
+	size_t size;
+	const void *data;
+};
+
+const struct storage_file *storage_files(void);
+
+#define storage_for_each_file(file_)					\
+	for ((file_) = storage_files(); (file_)->data; (file_)++)
+
+#endif /* INTERNAL_STORAGE_H */
