95bcd21f80ec71d06eb1f3eecc843e17ba0230ae
Fredrik Noring <noring@nocrew.org>
2022-09-10 08:31:21 +0200

    Move aes_find_object_shape to lib/gem/aes-shape.c

--- include/gem/aes-shape.h
+++ include/gem/aes-shape.h
@@ -11,4 +11,7 @@
 struct aes_area aes_object_shape_bounds(
 	struct aes_object_shape_iterator *iterator);
 
+bool aes_find_object_shape(struct aes_object_shape *shape,
+	const struct aes_point p, struct aes_object_shape_iterator *iterator);
+
 #endif /* _GEM_AES_SHAPE_H */
--- lib/gem/aes-shape.c
+++ lib/gem/aes-shape.c
@@ -22,3 +22,20 @@ struct aes_area aes_object_shape_bounds(
 
 	return bounds;
 }
+
+bool aes_find_object_shape(struct aes_object_shape *shape,
+	const struct aes_point p, struct aes_object_shape_iterator *iterator)
+{
+	struct aes_object_shape simple;
+	struct aes_object_shape s;
+	bool found = false;
+
+	aes_for_each_object_shape (&s, iterator)
+		aes_for_each_simple_object_shape (&simple, s)
+			if (aes_point_within_area(p, simple.area)) {
+				*shape = s;
+				found = true;
+			}
+
+	return found;
+}
